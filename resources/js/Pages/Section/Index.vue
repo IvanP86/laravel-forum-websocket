8669655865c9ceb411e31b96bbf075494ff98db7
IvanP86 <inthedark86@yandex.ru>
2024-04-11 16:06:46 +0500

    install lara, add sections, branches

---
+++ resources/js/Pages/Section/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="flex items-center">
+            <h3 class="text-xl mr-4">Разделы</h3>
+            <Link :href="route('sections.create')" class="block mr-4 w-1/6 px-2 py-1 bg-white border border-x-gray-300 rounded-lg text-center">+ Раздел</Link>
+            <Link :href="route('branches.create')" class="block w-1/6 px-2 py-1 bg-white border border-x-gray-300 rounded-lg text-center">+ Ветка</Link>
+        </div>
+    </div>
+</template>
+
+<script>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import {Link} from '@inertiajs/vue3';
+export default {
+    name: "Index",
+
+    components: {
+        Link
+    },
+
+    layout: MainLayout
+}
+</script>
+
+<style scoped>
+
+</style>
