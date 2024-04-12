b740d6243e400194f57f216541094990359720db
IvanP86 <inthedark86@yandex.ru>
2024-04-12 15:37:04 +0500

    index, show branch

--- resources/js/Pages/Section/Index.vue
+++ resources/js/Pages/Section/Index.vue
@@ -1,9 +1,23 @@
 <template>
     <div>
-        <div class="flex items-center">
+        <div class="flex items-center mb-8">
             <h3 class="text-xl mr-4">Разделы</h3>
             <Link :href="route('sections.create')" class="block mr-4 w-1/6 px-2 py-1 bg-white border border-x-gray-300 rounded-lg text-center">+ Раздел</Link>
             <Link :href="route('branches.create')" class="block w-1/6 px-2 py-1 bg-white border border-x-gray-300 rounded-lg text-center">+ Ветка</Link>
+        </div>
+        <div v-if="sections">
+            <div v-for="section in sections" class="mb-8">
+                <div v-if="section.branches.length">
+                    <div class="mb-4">
+                        <h3 class="text-lg">{{ section.title }}</h3>
+                    </div>
+                    <div v-for="branch in section.branches">
+                        <Link :href="route('branches.show', branch.id)" class=" block bg-white p-4 border border-gray-300">
+                            <h3>{{ branch.title }}</h3>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,6 +27,10 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 import {Link} from '@inertiajs/vue3';
 export default {
     name: "Index",
+
+    props: [
+        'sections'
+    ],
 
     components: {
         Link
