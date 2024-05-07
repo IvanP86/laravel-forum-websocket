20583108b7375b0a4e842d999dabf54c5af41629
IvanP86 <inthedark86@yandex.ru>
2024-05-07 11:55:55 +0500

    update UserWithRolesResource

--- app/Http/Resources/User/UserWithRolesResource.php
+++ app/Http/Resources/User/UserWithRolesResource.php
@@ -6,7 +6,7 @@ use App\Http\Resources\Role\RoleResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserWithrolesResource extends JsonResource
+class UserWithRolesResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
